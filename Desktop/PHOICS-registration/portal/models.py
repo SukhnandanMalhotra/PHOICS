15ae1e2176253607b11c654c8cdbdcd5aaa7fe17
bvishal8510 <baghel.vishal124124@gmail.com>
2017-09-17 18:14:45 +0530

    made some changes like gave a link for login page if user is not logged in and tries to access any innner page

--- Desktop/PHOICS-registration/portal/models.py
+++ Desktop/PHOICS-registration/portal/models.py
@@ -10,8 +10,12 @@ class Document(models.Model):
     status=models.CharField(max_length=7,choices=choice,default="PUBLIC")
     document = models.FileField( upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
     def __unicode__(self):
         return self.document
+
+    # def __str__(self):
+    #     return self.document
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,6 +23,9 @@ class Profile(models.Model):
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     email_confirmed = models.BooleanField(default=False)
+
+    # def __str__(self):
+    #     return self.user
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
