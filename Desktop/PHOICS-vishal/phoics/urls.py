f2c2402997730beba0834415fa23d257545ec793
bvishal8510 <baghel.vishal124124@gmail.com>
2017-09-19 20:37:57 +0530

    Displaying pictures completed

--- Desktop/PHOICS-vishal/phoics/urls.py
+++ Desktop/PHOICS-vishal/phoics/urls.py
@@ -1,35 +1,21 @@
-from django.conf.urls import url, include
-from django.contrib.auth import views as built_views
+from django.conf.urls import url
 from django.contrib import admin
-from portal import views
-
 from django.conf import settings
 from django.conf.urls.static import static
 
+from portal import views
+
+
 urlpatterns = [
-    # this url is for admin page "/admin/"
-    url(r'^admin/', admin.site.urls),
-    # this is for user loggin in page "/"
     url(r'^$', views.home, name='profile'),
     url(r'^upload/$', views.model_form_upload, name='model_form_upload'),
-    #
     url(r'^newsfeed/$', views.newsfeed, name='newsfeed'),
-    # this is for login page "/login"
-    url(r'^login/$', built_views.login, {'template_name': 'portal/login.html'}, name='login'),
-    # for logout but open login page
-    url(r'^logout/$', built_views.logout, {'next_page': 'login'}, name='logout'),
-    # signup page "/signup/"
-    url(r'^signup/$', views.signup, name='signup'),
-    # page after email send for verification "/account_activation_sent/"
-    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
-    # a link that send to user in email to activate your phoics account
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
-    # for front page "/front_page"
-    url(r'^front_page/$', views.front_page, name='front_page'),
-    # if user forget password then go "/forget_pass/"
-    url(r'^forget_pass/$', views.forget_pass, name='forget_pass'),
-
+    # url(r'^profilepic/$', views.profilepic, name='profilepic'),
+    url(r'^admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
--- Desktop/PHOICS-vishal/portal/views.py
+++ Desktop/PHOICS-vishal/portal/views.py
@@ -1,108 +1,39 @@
-"""
-This is a doc string
-"""
-from django.contrib.auth import login
-from django.core.mail import send_mail
-from django.core.exceptions import ValidationError
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from phoics.settings import EMAIL_HOST_USER
-from .forms import SignUpPage, forget_password
-from .tokens import account_activation_token
 from django.shortcuts import render, redirect
 
-from .models import Document
+from .models import Document,Comment
 from .forms import DocumentForm
 
-"""
- it will work when user is logged in
- it will redirect to the user on login page 
- having next parameter default have SETTING.LOGIN_URL
-"""
-@login_required
 def home(request):
-     documents = Document.objects.order_by('-uploaded_at')
-     return render(request,'portal/profile.html', {'documents': documents})
+    documents = Document.objects.all()
+    # profilepic=ProfilePic.objects.all()
+    posts=Comment.objects.all()
+    return render(request, 'portal/profile.html', {'documents': documents,'post':posts})
 
-# front page function which return front page html
-def front_page(request):
-    return render(request,'portal/front_page.html')
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpPage(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            User.objects.filter(email=email).count()
-            if email and User.objects.filter(email=email).count() > 0:
-                raise ValidationError('this email user already exist')
-            else:
-                user = form.save(commit=False)
-                user.is_active = False
-                user.save()
-                # get_current_site used to get the url of current page
-                current_site = get_current_site(request)
-                subject = 'Activate Your phoics Account'
-                message = render_to_string('portal/account_activation_email.html', {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                })
-                from_mail=EMAIL_HOST_USER
-                to_mail=[user.email]
-                send_mail(subject, message,from_mail,to_mail,fail_silently=False)
-                return redirect('account_activation_sent')
-    else:
-        form = SignUpPage()
-    return render(request, 'portal/signup.html', {'form': form})
-
-def account_activation_sent(request):
-    return render(request, 'portal/account_activation_sent.html')
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.profile.email_confirmed = True
-        user.save()
-        login(request, user)
-        return redirect('')
-    else:
-        return render(request, 'portal/account_activation_invalid.html')
 
 def newsfeed(request):
-    documents = Document.objects.order_by('-uploaded_at')
-    return render(request,'portal/newsfeed.html', {'documents': documents})
+    documents = Document.objects.all()
+    return render(request, 'portal/newsfeed.html', {'documents': documents})
+
+# def profilepic(request):
+#     if request.method == 'POST':
+#         form = ProfileImageForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('profile')
+#     else:
+#         form = ProfileImageForm()
+#     return render(request, 'portal/model_form_upload.html', {'form': form})
 
 def model_form_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            profile1 = form.save(commit=False)
+            profile1.user = request.user
+            profile1.save()
             return redirect('profile')
     else:
         form = DocumentForm()
     return render(request, 'portal/model_form_upload.html', {
         'form': form
     })
-
-def forget_pass(request):
-    if request.method == 'POST':
-        form = forget_password(request.POST)
-        if form.is_valid():
-            form.save(commit=False)
-            your_email = form.cleaned_data.get('your_email')
-            return redirect('front_page')
-
-    else:
-        form = forget_password()
-    return render(request, 'portal/forget_page.html',{'form':form})
